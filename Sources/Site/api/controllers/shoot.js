cfd18bda58b4ab7495632689568613d6b23ec098
mbonjour <mickael.bonjour@epfl.ch>
2017-02-01 09:09:47 +0100

    beginning controllers and set of API

--- Sources/Site/api/controllers/shoot.js
+++ Sources/Site/api/controllers/shoot.js
@@ -2,9 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/:idUser', function(req, res, next) {
     //TODO:On renvoit une liste des shoots récent a Vue
-    res.json({ title: 'Express' });
+    res.json({ title: 'Express', idUser: req.params.idUser});
 });
 
 module.exports = router;
