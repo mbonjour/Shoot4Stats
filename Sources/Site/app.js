43ca4de0c736da0ee7a6c6755bd7d519fe4ea430
mbonjour <mickael.bonjour@epfl.ch>
2017-03-20 11:37:11 +0100

    beginning passport implementation

--- Sources/Site/app.js
+++ Sources/Site/app.js
@@ -4,20 +4,42 @@ var logger = require('morgan')
 var cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser')
 var http = require('http')
+var passport = require('passport')
+var Strategy = require('passport-facebook').Strategy
+
+passport.use(new Strategy({
+  clientID: 'clientID',
+  clientSecret: 'clientSecret',
+  callbackURL: 'http://localhost:3000/login/facebook/return'
+},
+function (accessToken, refreshToken, profile, cb) {
+  return cb(null, profile)
+}))
+passport.serializeUser(function (user, cb) {
+  cb(null, user)
+})
+
+passport.deserializeUser(function (obj, cb) {
+  cb(null, obj)
+})
 
 var shoots = require(path.join(__dirname, 'api/controllers/shootsController.js'))
 var ends = require(path.join(__dirname, 'api/controllers/endsController.js'))
+var auth = require(path.join(__dirname, 'api/controllers/authController.js'))
 
 var app = express()
 
 app.use(logger('dev'))
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'static')))
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.use('/api/shoots', shoots)
 app.use('/api/ends', ends)
+app.use('/login', auth)
 
 app.use(function(req, res, next) {
   const err = new Error('Not Found')
