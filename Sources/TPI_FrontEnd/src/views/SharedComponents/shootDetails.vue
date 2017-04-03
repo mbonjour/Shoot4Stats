7185a8c2c7289da9aefc6e70c45cc244dc2be021
mbonjour <mic.bonjour@hotmail.fr>
2017-04-03 22:39:22 +0200

    add details in summary and details

--- Sources/TPI_FrontEnd/src/views/SharedComponents/shootDetails.vue
+++ Sources/TPI_FrontEnd/src/views/SharedComponents/shootDetails.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="shootDetails">
     <!-- Mettre un composant consacrée aux Stats -->
-    {{ this.$store.getters.currentShoot.nb_ends }}
-  </div
+    Tens : {{ this.$store.getters.currentShoot.tens }}</br>
+    Nines: {{ this.$store.getters.currentShoot.nines }}</br>
+    Gold Hitting percentage : {{ this.$store.getters.currentShoot.gold_hit }}%</br>
+    <div class="progress"><div class="determinate" :style="styleGoldHit"></div></div>
+    Total des points : {{ this.$store.getters.currentShoot.total }}</br>
+    Average Arrow : {{ this.$store.getters.currentShoot.average_arrow }}</br>
+  </div>
 </template>
+<script>
+export default {
+  computed: {
+    styleGoldHit () {
+      return 'width: ' + this.$store.getters.currentShoot.gold_hit + '%'
+    }
+  }
+}
+</script>
--- Sources/TPI_FrontEnd/src/views/SharedComponents/shootSummary.vue
+++ Sources/TPI_FrontEnd/src/views/SharedComponents/shootSummary.vue
@@ -6,11 +6,11 @@
       </div>
       <div class="card-stacked">
         <div class="card-content">
-          <div class="tout-dedans">
-            <p>{{ shoot.title }}</p>
-            <i class="material-icons" @click="toggleDetails">info</i>
-          </div>
-            <shootDetails v-if="detailVisibility"></shootDetails>
+          <div><strong>{{ this.shoot.title }}</strong>        {{ this.shoot.date }}</div>
+          <div>{{ this.shoot.description }}</div>
+          <div>Nbre flèches : {{ this.shoot.nb_ends*this.shoot.nb_arrows_by_end }}/{{ this.shoot.nb_total_ends*this.shoot.nb_arrows_by_end }}</div>
+          <div class="i-details"><i class="material-icons" @click="toggleDetails">{{ iconToShow }}</i>
+          <shootDetails v-if="detailVisibility"></shootDetails>
         </div>
       </div>
     </div>
@@ -41,16 +41,22 @@ export default {
   computed: {
     borderColor () {
       // Mettre images en local
-      return this.shoot.finished ? 'https://dummyimage.com/10x100/69f054/fff.png&text=+' : 'https://dummyimage.com/10x100/e34245/0011ff.png&text=+'
+      return this.shoot.finished ? 'https://dummyimage.com/10x150/69f054/fff.png&text=+' : 'https://dummyimage.com/10x150/e34245/0011ff.png&text=+'
+    },
+    iconToShow () {
+      return this.shoot.finished ? 'info' : 'mode_edit'
     }
   }
 }
 </script>
 
-<style>
-#tout-dedans.i {
+<style scoped>
+#tout-dedans {
+  position: relative;
+}
+#i-details {
   position: absolute;
-  right: 0px;
-  padding: 3px;
+  left: 0px;
+  top: 0px;
 }
 </style>
