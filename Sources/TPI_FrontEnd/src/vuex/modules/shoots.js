299208b5d128f8b3566c75a75b2daef56c030741
mbonjour <mic.bonjour@gmail.com>
2017-04-12 15:24:57 +0200

    add Shoot in VueX (not used)

--- Sources/TPI_FrontEnd/src/vuex/modules/shoots.js
+++ Sources/TPI_FrontEnd/src/vuex/modules/shoots.js
@@ -1,8 +1,10 @@
 import axios from 'axios'
+import { EventBus } from '../../helpers/event-bus.js'
 
 export default {
   state: {
-    shoots: []
+    shoots: [],
+    addedShoot: {}
   },
   mutations: {
     LOAD_SHOOTS (state) {
@@ -10,11 +12,29 @@ export default {
       .then((response) => {
         state.shoots = response.data
       })
+    },
+    ADD_SHOOT (state, shootObj) {
+      axios.post('/api/shoots', shootObj)
+        .then((response) => {
+          state.addedShoot = response.data
+        })
+        .catch((err) => {
+          EventBus.$emit('toastError', err + ' Veuillez contacter l\'admin si cela se reproduit')
+          console.log(err)
+        })
     }
   },
   actions: {
     loadShoots ({commit}) {
       commit('LOAD_SHOOTS')
+    },
+    addShoot ({commit}, shootObj) {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          commit('ADD_SHOOT', shootObj)
+          resolve()
+        }, 500)
+      })
     }
   },
   getters: {
