cffff3a094c807efa12b52ace65dbc515136b1a3
mbonjour <mic.bonjour@gmail.com>
2017-05-01 17:09:58 +0200

    false infinite Scroll integration

--- Sources/TPI_FrontEnd/src/views/Dashboard/dashboard.vue
+++ Sources/TPI_FrontEnd/src/views/Dashboard/dashboard.vue
@@ -6,7 +6,12 @@
       <p>Please go to <router-link to="/createShoot">Create a Shoot</router-link> or click the "plus" button to begin !!!</p>
     </div>
     <div class="row">
-      <shootItem v-for="currentShoot in shoots" :key="currentShoot.id" :shoot="currentShoot"></shootItem>
+      <shootItem v-for="currentShoot in list" :key="currentShoot.id" :shoot="currentShoot"></shootItem>
+      <infinite-loading :on-infinite="onInfinite" ref="infiniteLoading">
+        <span slot="no-more">
+          There is no more Shoots :(
+        </span>
+      </infinite-loading>
     </div>
     <div class="fixed-action-btn toolbar">
       <a class="btn-floating btn-large" style="background-color: #41B883;">
@@ -22,20 +27,44 @@
 </template>
 
 <script>
+import InfiniteLoading from 'vue-infinite-loading'
 import shootItem from '../SharedComponents/shootItem'
 import { mapGetters } from 'vuex'
 
 export default {
-  components: { shootItem },
+  components: { shootItem, InfiniteLoading },
   name: 'dashboard',
+  data () {
+    return {
+      list: [],
+      count: 0
+    }
+  },
+  methods: {
+    onInfinite () {
+      setTimeout(() => {
+        let currentCount = this.count
+        for (this.count; this.count < currentCount + 10; this.count++) {
+          console.log(this.shoots[this.count])
+          if (this.shoots[this.count]) {
+            this.list.push(this.shoots[this.count])
+          } else {
+            this.$refs.infiniteLoading.$emit('$InfiniteLoading:complete')
+            break
+          }
+        }
+        this.$refs.infiniteLoading.$emit('$InfiniteLoading:loaded')
+      }, 1000)
+    }
+  },
   created () {
     this.$store.dispatch('loadShoots')
   },
   computed: {
+    ...mapGetters(['shoots']),
     areShootsEmpty () {
       return this.shoots.length === 0
-    },
-    ...mapGetters(['shoots'])
+    }
   }
 }
 </script>
